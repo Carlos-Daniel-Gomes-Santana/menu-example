311cfe07f6e15aa2ec69a7d9592064ccc5dc18aa
Carlos Daniel Gomes Santana <carlos.daniel.programmer@gmail.com>
2022-07-12 19:07:28 -0300

    Created : new scene added
    Added the menu scene is only a scratch about it

--- main.lua
+++ main.lua
@@ -45,6 +45,7 @@ function love.load()
 end
 
 function love.update(dt)
+    
 end
 
 function love.draw()
--- scenes/menu.lua
+++ scenes/menu.lua
@@ -1,7 +1,47 @@
 local menu = {}
 
 function menu.create_scene()
-    
+    start = love.graphics.newText(Fonts.mid_font, "START")
+    love.graphics.draw(
+        start,
+        (Width - 210)/2,
+        (Height - 75)/2 - 175
+    )
+    love.graphics.rectangle(
+        "line",
+        (Width - 210)/2,
+        (Height - 75)/2 - 175,
+        210,
+        75
+    )
+
+    options = love.graphics.newText(Fonts.mid_font, "OPTIONS")
+    love.graphics.draw(
+        options,
+        (Width - 210)/2,
+        (Height - 75)/2
+    )
+    love.graphics.rectangle(
+        "line",
+        (Width - 210)/2,
+        (Height - 75)/2,
+        210,
+        75
+    )
+
+    exit = love.graphics.newText(Fonts.mid_font, "EXIT")
+    love.graphics.draw(
+        exit,
+        (Width - 210)/2 + exit:getWidth(),
+        (Height - 75)/2 + 175
+    )
+    love.graphics.rectangle(
+        "line",
+        (Width - 210)/2,
+        (Height - 75)/2 + 175,
+        210,
+        75
+    )
 end
 
 return menu
